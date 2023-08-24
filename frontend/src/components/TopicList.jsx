6ad7a82e26c12287bdd9c020e3d7148e4bccb179
Sourav <souravbadhan909@gmail.com>
2023-08-24 00:22:12 -0400

    A component that lists various topics.

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -4,19 +4,29 @@ import "../styles/TopicList.scss";
 
 const sampleDataForTopicList = [
   {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
+    "id": "1",
+    "slug": "topic-1",
+    "title": "Nature"
+  },  
+  {
+    "id": "2",
+    "slug": "topic-2",
+    "title": "Travel"
   },
   {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
+    "id": "3",
+    "slug": "topic-3",
+    "title": "People"
   },
   {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
+    "id": "4",
+    "slug": "topic-4",
+    "title": "Fashion"
+  },  
+  {
+    "id": "5",
+    "slug": "topic-5",
+    "title": "Animals"
   },
 ];
 
