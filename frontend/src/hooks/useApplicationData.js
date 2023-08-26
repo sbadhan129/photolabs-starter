dbb73897a2f2576068c1d686dc5b13eeec6ed191
Sourav <souravbadhan909@gmail.com>
2023-08-26 14:48:42 -0400

    A custom hook using the useReducer to manage the app's primary. To centralize state management and provide various state-updating actions and fetching data from the api.

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,29 +1,47 @@
 // hooks/useApplicationData.js
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 
 // 1. Define the initial state
 const initialState = {
   isModalOpen: false,
   selectedPhoto: null,
   favorites: [],
+  photoData: [],
+  topicData: []
 };
+
+const ACTIONS = {
+  SET_MODAL_OPEN: 'SET_MODAL_OPEN',
+  SET_SELECTED_PHOTO: 'SET_SELECTED_PHOTO',
+  UPDATE_FAVORITES: 'UPDATE_FAVORITES',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+};
+
 
 // 2. Define the reducer function
 const applicationDataReducer = (state, action) => {
   switch (action.type) {
-    case 'SET_MODAL_OPEN':
+    case ACTIONS.SET_MODAL_OPEN:
       return { ...state, isModalOpen: action.isOpen };
 
-    case 'SET_SELECTED_PHOTO':
+    case ACTIONS.SET_SELECTED_PHOTO:
       return { ...state, selectedPhoto: action.photo };
 
-    case 'UPDATE_FAVORITES':
+    case ACTIONS.UPDATE_FAVORITES:
       return {
         ...state,
         favorites: state.favorites.includes(action.photoId) 
           ? state.favorites.filter(id => id !== action.photoId) 
           : [...state.favorites, action.photoId]
       };
+
+    case ACTIONS.SET_PHOTO_DATA:
+      return { ...state, photoData: action.payload };
+
+      
+    case ACTIONS.SET_TOPIC_DATA:
+      return { ...state, topicData: action.payload };
 
     default:
       throw new Error(`Unsupported action type: ${action.type}`);
@@ -49,6 +67,37 @@ function useApplicationData() {
   const onClosePhotoDetailsModal = () => {
     setModalOpen(false);
   };
+
+  useEffect(() => {
+  // Define our fetch functions
+  const fetchPhotos = fetch("/api/photos").then(response => {
+    if (!response.ok) {
+      throw new Error("Failed to fetch photos.");
+    }
+    return response.json();
+  });
+
+  const fetchTopics = fetch("/api/topics").then(response => {
+    if (!response.ok) {
+      throw new Error("Failed to fetch topics.");
+    }
+    return response.json();
+  });
+
+  // Use Promise.all to execute both promises
+  Promise.all([fetchPhotos, fetchTopics])
+    .then(([photoData, topicData]) => {
+      console.log(photoData);  // Logging photo data
+      console.log(topicData);  // Logging topic data
+
+      dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: photoData });
+      dispatch({ type: ACTIONS.SET_TOPIC_DATA, payload: topicData });
+    })
+    .catch(error => {
+      console.error("There was a problem fetching data:", error);
+    });
+}, []);
+ 
 
   return {
     state,
