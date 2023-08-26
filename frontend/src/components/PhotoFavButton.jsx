ab40eee397410d1dfa92794b29fc13cd183a1362
Sourav <souravbadhan909@gmail.com>
2023-08-26 07:12:26 -0400

    A button encapsulating the favorite icon for each photo.To allow users to favorite/unfavorite photos with comments.

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,17 +4,24 @@ import '../styles/PhotoFavButton.scss';
 import { usePhotoContext } from './PhotoContext'; // Assuming you have this file and the mentioned exports
 
 function PhotoFavButton({ photoId, click }) {
+
+  // Access the favorites and its setter function from the context.
   const { favorites, setFavorites } = usePhotoContext();
   
   const handleFavClick = useCallback((event) => {
+    // Prevent the event from bubbling up to parent elements.
     event.stopPropagation();  
+    // Check if the photo is already favorited.
     const isFavorited = favorites.includes(photoId);
 
+    // Toggle the photo's favorite status based on its current status.
     if (isFavorited) {
       setFavorites(prevFavorites => prevFavorites.filter(id => id !== photoId));
     } else {
       setFavorites(prevFavorites => [...prevFavorites, photoId]);
     }
+
+    // Log the updated favorites for debugging purposes.
     console.log("Updated favorites:", favorites);
   }, [photoId, favorites, setFavorites]);
 
