0b3edfa0233d0394ccb62263eec6074d1f2d9a9a
Sourav <souravbadhan909@gmail.com>
2023-08-26 07:08:57 -0400

    It displays the FavIcon based on whether there's a favorite photo or not with comments

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/FavBadge.scss';
 
+//This component renders a badge for favorites. 
+
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
     <div className='fav-badge'>
