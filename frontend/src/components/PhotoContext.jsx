92adf2ac9afbb76dd13cc17235a172143a971fb1
Sourav <souravbadhan909@gmail.com>
2023-08-26 07:11:02 -0400

    Context providers for photo-related data. To provide and manage state (like favorites) across the app components without prop drilling with comments.

--- frontend/src/components/PhotoContext.jsx
+++ frontend/src/components/PhotoContext.jsx
@@ -1,11 +1,14 @@
 import React, { createContext, useState, useContext } from 'react';
 
+// Create a new context for photos
 const PhotoContext = createContext();
 
 export const PhotoProvider = ({ children }) => {
+    // Local state to hold the list of favorite photos
     const [favorites, setFavorites] = useState([]);
     
     return (
+        // Provide the favorites and its setter to the children
         <PhotoContext.Provider value={{ favorites, setFavorites }}>
             {children}
         </PhotoContext.Provider>
