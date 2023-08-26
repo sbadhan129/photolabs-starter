3b133eca492330431989069343ae7a35458a5ab2
Sourav <souravbadhan909@gmail.com>
2023-08-26 14:51:36 -0400

    The main homepage route. To display the app's primary content: top navigation and photo list.

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+// Import necessary components and hooks
 import TopNavigation from '../components/TopNavigationBar';
 import PhotoList from '../components/PhotoList';
 import { usePhotoContext } from '../components/PhotoContext';  // Assuming you've set up this context
 import '../styles/HomeRoute.scss';
 
-
+// Define the HomeRoute component
 const HomeRoute = ({ photos, topics, openModal, updateToFavPhotoIds }) => {
   const { favorites } = usePhotoContext();
 console.log("favorites in home", favorites)
 
   return (
     <div className="home-route">
+
+      {/* Render the TopNavigation component and pass down topics as props */}
       <TopNavigation topics={topics} />
       <PhotoList photos={photos} setFavorite={updateToFavPhotoIds} favorites={favorites} onPhotoClick={openModal} />
     </div>
