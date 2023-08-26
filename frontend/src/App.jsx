bcbea785f551401e0eb302dc91466bffb5507539
Sourav <souravbadhan909@gmail.com>
2023-08-26 07:05:39 -0400

    Added modal functionality to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import HomeRoute from 'routes/HomeRoute';
-import photos from './mocks/photos';
-import topics from './mocks/topics';
 import { PhotoProvider } from './components/PhotoContext';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import useApplicationData from './hooks/useApplicationData';
 
 const App = () => {
+  // Destructure the needed functions and state variables from the useApplicationData custom hook
   const { 
     state, 
     setModalOpen, 
@@ -15,23 +14,30 @@ const App = () => {
     onClosePhotoDetailsModal 
   } = useApplicationData();
 
-  const similarPhotos = photos.filter(p => p.id !== state.selectedPhoto?.id);
+  // Filter out the currently selected photo to get a list of similar photos
+  const similarPhotos = state.photoData.filter(p => p.id !== state.selectedPhoto?.id);
 
   return (
+     // Wrap the application within the PhotoProvider to make photo-related data and functions accessible to child components
     <PhotoProvider>
       <div className="App">
+
+        {/* Render the HomeRoute component and pass down necessary data and handlers as props */}
         <HomeRoute 
-          photos={photos} 
-          topics={topics} 
+          photos={state.photoData} 
+          topics={state.topicData} 
+
+           // Define the function to open the modal and set the selected photo
           openModal={(photo) => { setSelectedPhoto(photo); setModalOpen(true); }}
          updateToFavPhotoIds={updateToFavPhotoIds}
         />
+
+         {/* Conditionally render the PhotoDetailsModal based on whether the modal should be open */}
         {state.isModalOpen && (
           <PhotoDetailsModal 
             photo={state.selectedPhoto} 
             similarPhotos={similarPhotos} 
             closeModal={onClosePhotoDetailsModal}
-           
           />
         )}
       </div>
