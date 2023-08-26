c0d9e4f6cee46e2d5e062ed9053c0bf2d34745cc
Sourav <souravbadhan909@gmail.com>
2023-08-26 14:43:56 -0400

    To display the name of a specific topic. Individual topic list item. and added comments

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,18 +3,29 @@ import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = ({ id, user, urls, location, click, isFavorite, onPhotoClick }) => {
+  
+  // Destructuring to get the necessary properties from the user object.
   const { username, profile } = user;
+
+  // Extract the regular resolution image URL for display.
   const imageSource = urls.regular;
 
   return (
     
     <div className="photo-list-item" data-id={id} onClick={onPhotoClick}>
+      
+      {/* Favorite button to mark/unmark photo as favorite */}
       <PhotoFavButton photoId={id} click={click}/>
+
+
+      {/* The main photo image */}
       <img 
         src={imageSource} 
         alt={`Photo taken in ${location.city}, ${location.country}`} 
         className="photo-list-image" 
       />
+
+      {/* User details section - profile image, username, and location */}
       <div className="photo-list-user-details">
         <img 
           src={profile} 
@@ -28,6 +39,7 @@ const PhotoListItem = ({ id, user, urls, location, click, isFavorite, onPhotoCli
           </div>
         </div>
       </div>
+      {/* Display a heart icon if the photo is marked as a favorite */}
       {isFavorite && <span>❤️</span>}
     </div>
   );
