6b78bfc51ea481f17027fb892e69a18c99960c9a
Sourav <souravbadhan909@gmail.com>
2023-08-26 21:19:47 -0400

    A modal that provides details of a selected photo and another version in which you can see another pic at bottom for comparison and shows similar photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -5,7 +5,11 @@ import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = ({ closeModal, photo, similarPhotos }) => {
   console.log(photo)
-  
+  const [currentPhoto, setCurrentPhoto] = React.useState(photo);
+
+  const updateModalContent = (newPhoto) => {
+    setCurrentPhoto(newPhoto);
+  };
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={closeModal}>
@@ -15,11 +19,17 @@ const PhotoDetailsModal = ({ closeModal, photo, similarPhotos }) => {
       {/* Displaying the larger version of the selected photo */}
       <img src={photo.urls.full} alt={`Large view of photo taken in ${photo.location.city}, ${photo.location.country}`} className="photo-details-modal__image" />
 
+      <img src={currentPhoto.urls.full} 
+       alt={`Large view of photo taken in ${currentPhoto.location.city}, ${currentPhoto.location.country}`} 
+       className="photo-details-modal__image" />
+
       {/* Adding a heading for similar photos */}
       <h2 className="photo-details-modal__similar-heading">Similar Photos</h2>
 
       {/* Displaying similar photos */}
-      <PhotoList photos={similarPhotos}  />
+      <PhotoList photos={similarPhotos}  
+      onPhotoClick={updateModalContent}
+      />
 
     </div>
   )
