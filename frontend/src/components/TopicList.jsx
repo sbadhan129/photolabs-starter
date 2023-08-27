40f3305db9e7e0f0a4a345e01f39938676c7adae
Sourav <souravbadhan909@gmail.com>
2023-08-26 21:24:13 -0400

    To display a collection of photos with details like the user who uploaded the photo, location,etc and added comments in the code and removed the sample data.

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,39 +2,39 @@ import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    "id": "1",
-    "slug": "topic-1",
-    "title": "Nature"
-  },  
-  {
-    "id": "2",
-    "slug": "topic-2",
-    "title": "Travel"
-  },
-  {
-    "id": "3",
-    "slug": "topic-3",
-    "title": "People"
-  },
-  {
-    "id": "4",
-    "slug": "topic-4",
-    "title": "Fashion"
-  },  
-  {
-    "id": "5",
-    "slug": "topic-5",
-    "title": "Animals"
-  },
-];
-
-const TopicList = () => {
+const sampleDataForTopicList = 
+  [
+    {
+    id: 1,
+    title: "People",
+    slug: "people"
+    },
+    {
+    id: 2,
+    title: "Nature",
+    slug: "nature"
+    },
+    {
+    id: 3,
+    title: "Travel",
+    slug: "travel"
+    },
+    {
+    id: 4,
+    title: "Animals",
+    slug: "animals"
+    },
+    {
+    id: 5,
+    title: "Fashion",
+    slug: "fashion"
+    }
+    ];
+const TopicList = ({fetchPhotosByTopic}) => {
   return (
     <div className="top-nav-bar__topic-list">
       {sampleDataForTopicList.map((topic) => (
-        <TopicListItem key={topic.id} label={topic.title}  />
+        <TopicListItem key={topic.id} id={topic.id} label={topic.title} fetchPhotosByTopic = {fetchPhotosByTopic}  />
       ))}
     </div>
   );
