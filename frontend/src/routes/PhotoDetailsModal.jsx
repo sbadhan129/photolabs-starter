aad1d9daee485399368a426c368ce5379df967e8
Sourav <souravbadhan909@gmail.com>
2023-08-28 16:48:04 -0400

    Changed the position of the buttons in the component.

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/PhotoDetailsModal.scss';
 import PhotoList from '../components/PhotoList';
+import PhotoFavButton from '../components/PhotoFavButton';
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = ({ closeModal, photo, similarPhotos }) => {
@@ -12,16 +13,18 @@ const PhotoDetailsModal = ({ closeModal, photo, similarPhotos }) => {
   };
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={closeModal}>
+
+<button className="photo-details-modal__close-button" onClick={closeModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      
-      {/* Displaying the larger version of the selected photo */}
-      <img src={photo.urls.full} alt={`Large view of photo taken in ${photo.location.city}, ${photo.location.country}`} className="photo-details-modal__image" />
 
-      <img src={currentPhoto.urls.full} 
-       alt={`Large view of photo taken in ${currentPhoto.location.city}, ${currentPhoto.location.country}`} 
-       className="photo-details-modal__image" />
+       <div className="photo-details-modal__image-wrapper">
+           <img src={currentPhoto.urls.full} 
+         alt={`Large view of photo taken in ${currentPhoto.location.city}, ${currentPhoto.location.country}`} 
+      className="photo-details-modal__image" 
+    />
+    <PhotoFavButton photoId={currentPhoto.id} />
+</div>
 
       {/* Adding a heading for similar photos */}
       <h2 className="photo-details-modal__similar-heading">Similar Photos</h2>
