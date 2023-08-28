bc26c32ab85cfc798d59188817478070d45a30bf
Sourav <souravbadhan909@gmail.com>
2023-08-28 16:45:38 -0400

    rendered topics in <topiclist/>

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -6,14 +6,24 @@ import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
 // Functional component for the top navigation bar.
-const TopNavigation = ({fetchPhotosByTopic}) => {
-  const { favorites } = usePhotoContext();
+const TopNavigation = ({topics,fetchPhotosByTopic}) => {
+  const { favorites, darkMode, setDarkMode } = usePhotoContext();
  
 
   // useEffect hook to perform side effects.
   // In this case, it logs the number of favorites every time they change.
+  
   useEffect(() => {
-    console.log('Favorites in Top Navigation:', favorites);
+    // Use effect to add/remove a class on the document body for dark mode
+    if (darkMode) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+}, [darkMode]);
+  
+  
+  useEffect(() => {
   }, [favorites])  // Dependency array to rerun the effect when 'favorites' change.
 
   // Determines if there are any favorite photos. 
@@ -24,8 +34,13 @@ const TopNavigation = ({fetchPhotosByTopic}) => {
     <div className="top-nav-bar">
       {/* Brand/logo for the application. */}
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList fetchPhotosByTopic = {fetchPhotosByTopic} />
+      <TopicList topics={topics} fetchPhotosByTopic = {fetchPhotosByTopic} />
       <FavBadge isFavPhotoExist={displayAlert} />
+      <div>
+        <button className="button-toggle-mode" onClick={() => setDarkMode(!darkMode)}>
+            {darkMode ? "Light Mode" : "Dark Mode"}
+        </button>
+    </div>
     </div>
   )
 }
