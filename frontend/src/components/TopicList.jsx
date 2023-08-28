aeaeee64f210fc0389f051a823038fefc06c7988
Sourav <souravbadhan909@gmail.com>
2023-08-28 16:37:09 -0400

    removed the hardcoded data from the component.

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,43 +1,14 @@
 import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
-
-const sampleDataForTopicList = 
-  [
-    {
-    id: 1,
-    title: "People",
-    slug: "people"
-    },
-    {
-    id: 2,
-    title: "Nature",
-    slug: "nature"
-    },
-    {
-    id: 3,
-    title: "Travel",
-    slug: "travel"
-    },
-    {
-    id: 4,
-    title: "Animals",
-    slug: "animals"
-    },
-    {
-    id: 5,
-    title: "Fashion",
-    slug: "fashion"
-    }
-    ];
     
-const TopicList = ({fetchPhotosByTopic}) => {
-  return (
-    <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topic) => (
-        <TopicListItem key={topic.id} id={topic.id} label={topic.title} fetchPhotosByTopic = {fetchPhotosByTopic}  />
-      ))}
-    </div>
+const TopicList = ({topics, fetchPhotosByTopic}) => {
+    return (
+      <div className="top-nav-bar__topic-list">
+        {topics.map((topic) => (
+          <TopicListItem key={topic.id} id={topic.id} label={topic.title} fetchPhotosByTopic={fetchPhotosByTopic} />
+        ))}
+      </div>
   );
 };
 
